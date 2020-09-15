ef113aec745d99313165dad35b1faa46ded51724
penley.xu <penley.xu@williamoneilchina.com>
2020-09-15 18:38:57 +0800

    update the yScale

--- src/scale/ScaleY.js
+++ src/scale/ScaleY.js
@@ -1,14 +1,87 @@
 class ScaleY {
-  createScale (scaleConfig, top, bottom) {
+  createScale (scaleConfig, timeSeries, top, bottom) {
+    let yScale = null
+    const { minVal, maxVal } = this.getValueDomain(timeSeries)
+
+    let domain = [minVal, maxVal]
+    const range = [bottom, top]
+
+    if (!['linear', 'log', 'pow', 'auto'].includes(scaleConfig.type)) {
+      throw new Error(`Do not support ${scaleConfig.type} yet.`)
+    }
+
     switch (scaleConfig.type) {
-      case 'log':
-        break
       case 'linear':
+        yScale = this.getScaleMap(scaleConfig, domain, range)
+        break
+      case 'log':
+        if (scaleConfig.logBase < 1) domain = [maxVal, minVal]
+        yScale = this.getScaleMap(scaleConfig, domain, range)
         break
       case 'pow':
+        if (scaleConfig.pow < 0) domain = [maxVal, minVal]
+        yScale = this.getScaleMap(scaleConfig, domain, range)
+        break
+      case 'auto':
+        yScale = this.getScaleMap(scaleConfig, domain, range)
+        break
+    }
+
+    return {
+      yScale
+    }
+  }
+
+  getScaleMap (scaleConfig, domain, range, clamp) {
+    let transformer = null
+    switch (scaleConfig.type) {
+      case 'linear':
+        transformer = (d) => d
+        break
+      case 'log':
+        transformer = (d) => Math.log(d) / Math.log(scaleConfig.logBase)
+        break
+      case 'pow':
+        transformer = (d) => Math.pow(d, scaleConfig.powExponent)
+        break
+      case 'auto':
+        transformer = (d) => Math.log10(d)
         break
       default:
+        transformer = (d) => d
         break
+    }
+
+    const k = (range[1] - range[0]) / (transformer(domain[1]) - transformer(domain[0]))
+    const b = range[0] - k * transformer(domain[0])
+
+    return (x) => {
+      const result = k * x + b
+      if (clamp !== undefined && result > range[1]) return range[1]
+      if (clamp !== undefined && result < range[0]) return range[0]
+      return result
+    }
+  }
+
+  getValueDomain (timeSeries) {
+    let minVal
+    let maxVal
+
+    const isLegal = v => v !== null && typeof v !== 'undefined'
+    const ts = timeSeries.prices.filter(p => isLegal(p.close) && isLegal(p.high) && isLegal(p.low))
+
+    for (const v of ts) {
+      if (minVal === undefined || minVal > v.low) {
+        minVal = v.low
+      }
+      if (maxVal === undefined || maxVal < v.high) {
+        maxVal = v.high
+      }
+    }
+
+    return {
+      minVal,
+      maxVal
     }
   }
 }
